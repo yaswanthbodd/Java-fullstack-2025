a28d6c80221ee6cddbe1515255f7620995ecb079
yaswanthbodd <bodduyaswanth09@gmail.com>
2025-05-19 15:21:02 +0530

    Hibernate-ProductProjInsert

---
+++ Hibernate/ProductProj/src/main/java/com/product/Product.java
@@ -0,0 +1,26 @@
+package com.product;
+
+public class Product {
+	private int prodId;
+	private String prodName;
+	private double prodPrice;
+	public int getProdId() {
+		return prodId;
+	}
+	public void setProdId(int prodId) {
+		this.prodId = prodId;
+	}
+	public String getProdName() {
+		return prodName;
+	}
+	public void setProdName(String prodName) {
+		this.prodName = prodName;
+	}
+	public double getProdPrice() {
+		return prodPrice;
+	}
+	public void setProdPrice(double prodPrice) {
+		this.prodPrice = prodPrice;
+	}
+	
+}
--- Hibernate/ProductProj/src/main/java/com/product/ProductInsert.java
+++ Hibernate/ProductProj/src/main/java/com/product/ProductInsert.java
@@ -1,21 +1,20 @@
-package com.student;
+package com.product;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 
-public class StudentInsert {
+public class ProductInsert {
 
 	public static void main(String[] args) {
-		SessionFactory sessionFactory = StudentUtil.getSessionFactory();
+		SessionFactory sessionFactory = ProductUtil.getSessionFactory();
 		Session session = sessionFactory.openSession();
 		Transaction tx = session.beginTransaction();
-		Student stud = new Student();
-		stud.setStudentId(585);
-		stud.setStudentName("Harsha");
-		stud.setCollege("LBRCE");
-		stud.setAge(24);
-		session.persist(stud);
+		Product prod=new Product();
+		prod.setProdId(101);
+		prod.setProdName("Samsung");
+		prod.setProdPrice(9000.0);
+		session.persist(prod);
 		tx.commit();
 		session.close();
 	}
